ec6ab1b148a34b00195e668403e1e0b3bcf77bc6
Карташов Александр Евгеньевич <AKartashov2@alfabank.ru>
2022-08-12 18:00:09 +0300

    feat: added

--- src/main/java/com/sirius/koshelek/KoshelekApplication.java
+++ src/main/java/com/sirius/koshelek/KoshelekApplication.java
@@ -2,8 +2,9 @@ package com.sirius.koshelek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class KoshelekApplication {
 
     public static void main(String[] args) {
